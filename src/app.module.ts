674449cc320cbea550aaf86de6c3950679d5fcbe
graff012 <sobirjonovnuriddin43@gmail.com>
2025-05-03 22:29:33 +0500

    delete unnesessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
@@ -17,7 +15,7 @@ import { CommentsModule } from './comments/comments.module';
     ReviewsModule,
     CommentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
